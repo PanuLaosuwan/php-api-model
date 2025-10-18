4e6d3ba24c6b96ff998fdbac5cce320c7c0c5704
PanuLaosuwan <panu.laosuwan@gmail.com>
2025-10-18 19:23:22 +0700

    delete test file

--- mysqldb.php
+++ mysqldb.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "P@ssw0rd"; // ถ้ามีให้ใส่รหัสผ่าน
+$password = "8oojkiyd"; // ถ้ามีให้ใส่รหัสผ่าน P@ssw0rd
 $dbname = "apimodel"; // ชื่อฐานข้อมูล
 
 $conn = new mysqli($servername, $username, $password, $dbname);
